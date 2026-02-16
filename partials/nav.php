6fd43f706a04661ee4384e1f0e31b3df1f079562
aditya3560 <adityashelar3560@gmail.com>
2026-02-16 22:54:47 +0530

    Add files via upload

---
+++ partials/nav.php
@@ -0,0 +1,13 @@
+<div class="nav">
+  <div class="container">
+    <div class="row">
+      <div class="brand">🚦 NagarSeva Online</div>
+      <div class="nav-links">
+        <a href="/NagarSeva/index.php">Home</a>
+        <a href="/NagarSeva/register.php">Register</a>
+        <a href="/NagarSeva/login.php">User Login</a>
+        <a href="/NagarSeva/admin/login.php">Admin</a>
+      </div>
+    </div>
+  </div>
+</div>
